3ec38cdc70493c53e61d9d79d23c603236ccbfed
@dianavile <diana.vile@gmail.com>
2018-05-28 13:18:31 +0200

    Update AddressBook.js

--- ud549-master/src/AddressBook.js
+++ ud549-master/src/AddressBook.js
@@ -1,7 +1,11 @@
 function AddressBook () {
 	this.contacts = [];
+}
 
 	AddressBook.prototype.addContact = function (contact) {
 		this.contacts.push(contact);
-	}	
+}	
+
+AddressBook.prototype.getContact = function (index) {
+		this.contacts[index];
 }
