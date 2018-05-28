c9b634484f45ff1c54e37ee71ffd9836a57c6be3
@dianavile <diana.vile@gmail.com>
2018-05-28 15:12:18 +0200

    Update AddressBookSpec.js

--- ud549-master/spec/AddressBookSpec.js
+++ ud549-master/spec/AddressBookSpec.js
@@ -1,20 +1,23 @@
 describe('Address Book', function() {
-	it('should be able to add a contact', function() {
-		var addressBook = new AddressBook(),
+	var addressBook = new AddressBook();
 			thisContact = new Contact();
+	
+	beforeEach (function){
+
+	}); 	
+			
+	it('should be able to add a contact', function() {
+		
 
 			addressBook.addContact(thisContact);
 
 			expect(adressBook.getContact(0)).toBe(thisContact);
-	});
-	
-	it('should be able to delete a contact', function(){
-		var addressBook = new AddressBook(),
-		    thisContact = new Contact();
-		    
+	}); 
+
+	it('should be able to delete a contact', function() {
 			addressBook.addContact(thisContact);
-			addressBook.deleteContact(0);	
-		
+			addressBook.deleteContact(0);
+
 			expect(adressBook.getContact(0)).not.toBeDefined();
-	});
+	}); 	
 });
